a633473b5518282014418bfc232b2ca72dae618c
FunkiDunki <nicholashotelling@gmail.com>
2023-12-07 13:09:48 -0800

    navigate between registrations and search

--- packages/react-frontend/src/App.js
+++ packages/react-frontend/src/App.js
@@ -3,9 +3,29 @@ import Search from './Search.js'
 import Output from './OutputComponent.js'
 import React, { useState } from 'react'
 import Auth from './Auth.js'
+import Registrations from './Registrations.js'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 
 const Banner = () => {
     return <div className="banner">Poly Nook</div>
+}
+
+const SearchSection = (props) => {
+    return (
+        <div>
+            <Search handleSubmit={props.handleSearchSubmit} />
+            <div className="content">
+                <h1>Welcome to Poly Nook</h1>
+                <p>Your resource for finding study spaces!</p>
+                <p>{props.bodyText}</p>
+                {props.apiCallSuccessful ? (
+                    <Output objectList={props.objectList} />
+                ) : (
+                    <p></p>
+                )}
+            </div>
+        </div>
+    )
 }
 
 const SearchDatabase = (searchTerm) => {
@@ -21,7 +41,8 @@ const SearchDatabase = (searchTerm) => {
 }
 
 function App() {
-    const [bodyText, setBodyText] = useState('')
+    const [bodyText, setBodyText] = useState()
+    const [profile, setProfile] = useState(null)
     const [apiCallSuccessful, setApiCallSuccessful] = useState(false)
     const [objectList, setObjectList] = useState([])
     const handleSearchSubmit = (searchTerm) => {
@@ -66,18 +87,57 @@ function App() {
     return (
         <div className="App">
             <Banner />
-            <Search handleSubmit={handleSearchSubmit} />
-            <div className="content">
-                <h1>Welcome to Poly Nook</h1>
-                <p>Your resource for finding study spaces!</p>
-                <p>{bodyText}</p>
-                {apiCallSuccessful ? (
-                    <Output objectList={objectList} />
-                ) : (
-                    <p></p>
-                )}
-            </div>
-            <Auth />
+            <Auth profile={profile} setProfile={setProfile} />
+            <Router>
+                <nav
+                    style={{
+                        padding: '30px 10%',
+                    }}
+                >
+                    <ul>
+                        <li
+                            style={{
+                                display: 'inline-block',
+                                padding: '0px 20px',
+                            }}
+                        >
+                            <Link to="/">Search</Link>
+                        </li>
+                        <li
+                            style={{
+                                display: 'inline-block',
+                                padding: '0px 20px',
+                            }}
+                        >
+                            <Link to="/registrations">Registrations</Link>
+                        </li>
+                    </ul>
+                </nav>
+                <Routes>
+                    <Route
+                        exact
+                        path="/registrations"
+                        element={
+                            <Registrations
+                                profile={profile}
+                                registrations_list={[1, 2, 3]}
+                            />
+                        }
+                    />
+                    <Route
+                        exact
+                        path="/"
+                        element={
+                            <SearchSection
+                                handleSearchSubmit={handleSearchSubmit}
+                                apiCallSuccessful={apiCallSuccessful}
+                                objectList={objectList}
+                                bodyText={bodyText}
+                            />
+                        }
+                    />
+                </Routes>
+            </Router>
         </div>
     )
 }
--- packages/react-frontend/src/Registrations.js
+++ packages/react-frontend/src/Registrations.js
@@ -1,43 +1,8 @@
-import { useState } from 'react'
-import { googleLogout, GoogleLogin } from '@react-oauth/google'
-import { jwtDecode } from 'jwt-decode'
-
-function Registrations() {
-    const [profile, setProfile] = useState([])
-
-    function handleSignInResponse(response) {
-        response = jwtDecode(response.credential)
-        setProfile(response)
-    }
-
-    function logout() {
-        setProfile()
-        googleLogout()
-    }
-
+function Registrations(props) {
     return (
         <div>
-            <h1>GoogleLogin</h1>
-            {profile ? (
-                <div
-                    style={{
-                        display: 'flex-box',
-                    }}
-                >
-                    <img src={profile.picture} />
-                    <div>
-                        <p>Logged in as {profile.email}</p>
-                        <button onClick={logout}>Logout of account</button>
-                    </div>
-                </div>
-            ) : (
-                <GoogleLogin
-                    onSuccess={handleSignInResponse}
-                    onError={() => {
-                        console.log('login failed.')
-                    }}
-                />
-            )}
+            <h1>This is your registrations currently, {props.profile.name}</h1>
+            <p>{props.registrations_list}</p>
         </div>
     )
 }
